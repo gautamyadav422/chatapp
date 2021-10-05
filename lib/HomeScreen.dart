3136f5b97fa2fc9a514f986e3d8dca593b0873ba
gautamyadav422 <gautamyadav422@gmail.com>
2021-10-05 20:24:17 +0530

    Initial commit

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,27 @@
+import 'package:chatapp/Methods.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = Mediaq
+    return Scaffold(
+      body:Column(
+
+      )
+      // Center(
+      //   child: TextButton(
+      //     onPressed: () => logOut(context),
+      //     child: Text("LogOut"),
+      //   ),
+      // ),
+    );
+  }
+}
